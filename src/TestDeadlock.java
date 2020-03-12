07a2615d7c55e375c2468be6075d4bc9ee1cc385
titanmet <titanmet@mail.ru>
2020-03-12 17:08:15 +0300

    deadlock

---
+++ src/TestDeadlock.java
@@ -0,0 +1,118 @@
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestDeadlock {
+    public static void main(String[] args) throws InterruptedException {
+        Runners runners = new Runners();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runners.firstThread();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runners.secondThread();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        runners.finished();
+    }
+}
+
+class Runners {
+    private Account account1 = new Account();
+    private Account account2 = new Account();
+
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
+
+    private void takeLock(Lock lock1, Lock lock2) {
+        boolean firstLockTaken = false;
+        boolean secondLockTaken = false;
+        while (true) {
+            try {
+                firstLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if (firstLockTaken && secondLockTaken) {
+                    return;
+                }
+
+                if (firstLockTaken) {
+                    lock1.unlock();
+                }
+                if (secondLockTaken) {
+                    lock2.unlock();
+                }
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void firstThread() {
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            takeLock(lock1, lock2);
+            try {
+                Account.transfer(account1, account2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
+        }
+    }
+
+    public void secondThread() {
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            takeLock(lock2, lock1);
+            try {
+                Account.transfer(account2, account1, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
+        }
+    }
+
+    public void finished() {
+        System.out.println(account1.getBallance());
+        System.out.println(account2.getBallance());
+        System.out.println("Total balance " + (account1.getBallance() + account2.getBallance()));
+    }
+}
+
+class Account {
+    private int balance = 10000;
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public int getBallance() {
+        return balance;
+    }
+
+    public static void transfer(Account acc1, Account acc2, int amount) {
+        acc1.withdraw(amount);
+        acc2.deposit(amount);
+    }
+}
